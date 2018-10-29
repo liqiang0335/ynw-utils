572842b41ccaf24cbca0e3a2b3b27188d78fd7aa
liqiang <liqiang0335@qq.com>
2018-10-29 10:15:02 +0800

    add data structure

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 export { default as EventHub } from "./src/EventHub";
 export { default as debounce } from "./src/debounce";
+export { default as LinkedList } from "./src/dataStructure/LinkedList";
+export { default as HashTable } from "./src/dataStructure/HashTable";
+export { default as Queue } from "./src/dataStructure/Queue";
+export { default as Stack } from "./src/dataStructure/Stack";
+
 export * from "./src/chain";
