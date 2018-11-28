03a789d57998761450e6bde33d2a628c2a7a3ed5
liqiang <liqiang0335@qq.com>
2018-11-28 16:42:41 +0800

    add copy

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ export { default as LinkedList } from "./src/dataStructure/LinkedList";
 export { default as HashTable } from "./src/dataStructure/HashTable";
 export { default as Queue } from "./src/dataStructure/Queue";
 export { default as Stack } from "./src/dataStructure/Stack";
+import { default as copyText } from "./src/copy";
 
 export * from "./src/chain";
