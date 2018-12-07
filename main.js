c3fecb551bdd204136f67eba6d15d87c525d4700
liqiang <liqiang0335@qq.com>
2018-12-07 18:15:15 +0800

    add chian

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ export { default as Queue } from "./src/dataStructure/Queue";
 export { default as Stack } from "./src/dataStructure/Stack";
 
 //Chain
-export { default as applyMiddleware } from "./chain/applyMiddleware";
-export { default as compose } from "./chain/compose";
-export { default as composeRight } from "./chain/composeRight";
-export { default as koaChain } from "./chain/koaChain";
-export { default as nextChain } from "./chain/nextChain";
-export { default as promiseChain } from "./chain/promiseChain";
+export { default as applyMiddleware } from "./src/chain/applyMiddleware";
+export { default as compose } from "./src/chain/compose";
+export { default as composeRight } from "./src/chain/composeRight";
+export { default as koaChain } from "./src/chain/koaChain";
+export { default as nextChain } from "./src/chain/nextChain";
+export { default as promiseChain } from "./src/chain/promiseChain";
 
 //BOM
 export { default as copyText } from "./src/copy";
