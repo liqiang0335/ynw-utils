4829822d9475d289417a930d9d561266e1f1e52c
liqiang <liqiang0335@qq.com>
2019-04-26 10:59:18 +0800

    update

--- node/fs.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const util = require("util");
-
-exports.exists = util.promisify(fs.exists);
-exports.readFile = util.promisify(fs.readFile);
-exports.writeFile = util.promisify(fs.writeFile);
-exports.readdir = util.promisify(fs.readdir);
